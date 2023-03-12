33d6b65fb9fb07203d03b64bd712b7ce5771107c
Achmad Ali Akbar <114000864+barcodew@users.noreply.github.com>
2023-03-12 17:19:35 +0800

    Update ketupat_panjang.java

--- ketupat_panjang.java
+++ ketupat_panjang.java
@@ -2,25 +2,29 @@
 public class ketupad {
     public static void main(String[] args) {
         
-        for (int i = 0; i < 9; i++){
-            for (int j = 0; j < 9; j++){
-                if ( (i+j) > 12){
-                    break;
-                }
-                else if ( (j >= 4) && (j-i) > 4){
-                    break;
-                }
-                else if ( (j <= 4) && (i+j) < 4){
-                    System.out.print("  ");
-                    continue;
-                }
-                else if ( (i >= 4) && (i-j) > 4){
-                    System.out.print("  ");
-                    continue;
-                }
-                System.out.print("* ");
+        for (int i=1;i<=10;i++){
+            for(int j=9;j>=i;j--){
+                System.out.print(" ");
             }
-            System.out.print("\n");
+            for(int k=1;k<=i;k++){
+                System.out.print("*");
+            }
+            for (int l=1;l<=i-1;l++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = 2;i<=10;i++){
+            for(int j = 1;j<=i-1;j++){
+                System.out.print(" ");
+            }
+            for(int k=9;k>=i-1;k--){
+                System.out.print("*");
+            }
+            for (int l=9;l>=i;l--){
+                System.out.print("*");
+            }
+            System.out.println();
         }
 
     }
