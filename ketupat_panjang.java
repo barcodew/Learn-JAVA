546926f8b0ad8c8a8858cddec5349f3e27912a46
Achmad Ali Akbar <114000864+barcodew@users.noreply.github.com>
2023-03-12 17:18:36 +0800

    Create ketupat_panjang.java

---
+++ ketupat_panjang.java
@@ -0,0 +1,28 @@
+
+public class kotakkotak {
+    public static void main(String[] args) {
+        
+        for (int i = 0; i < 9; i++){
+            for (int j = 0; j < 9; j++){
+                if ( (i+j) > 12){
+                    break;
+                }
+                else if ( (j >= 4) && (j-i) > 4){
+                    break;
+                }
+                else if ( (j <= 4) && (i+j) < 4){
+                    System.out.print("  ");
+                    continue;
+                }
+                else if ( (i >= 4) && (i-j) > 4){
+                    System.out.print("  ");
+                    continue;
+                }
+                System.out.print("* ");
+            }
+            System.out.print("\n");
+        }
+
+    }
+    
+}
