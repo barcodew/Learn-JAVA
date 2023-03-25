ff5ef0890e9d3c07ebdb715567a9862f868e8404
Achmad Ali Akbar <114000864+barcodew@users.noreply.github.com>
2023-03-25 13:29:36 +0800

    Update Encrypt_Decrypt_CaesarAndVigenereCipher.java

--- Encrypt_Decrypt_CaesarAndVigenereCipher.java
+++ Encrypt_Decrypt_CaesarAndVigenereCipher.java
@@ -82,3 +82,54 @@ public class TugasNomer5 {
     System.out.println("Decrypted Vigenere Cipher: " + decryptedVigenereCipher);
   }   
 }
+
+// Algoritma Caesar Cipher
+// 
+// Langkah Manual:
+// 1. Tentukan kunci yang akan digunakan untuk mengenkripsi pesan.
+// 2. Cari posisi huruf dalam alfabet.
+// 3. Tambahkan kunci ke posisi huruf untuk menghasilkan posisi baru.
+// 4. Ganti huruf dengan posisi baru yang telah ditentukan.
+// 
+// Encrypt:
+// 1. Masukkan pesan teks yang akan dienkripsi.
+// 2. Masukkan kunci untuk Shift.
+// 3. Untuk setiap karakter dalam pesan teks, cari posisi dalam alfabet.
+// 4. Tambahkan kunci Shift ke posisi alfabet untuk menghasilkan posisi baru.
+// 5. Ganti karakter dengan posisi baru yang telah ditentukan.
+// 6. Tampilkan hasil enkripsi.
+// 
+// Decrypt:
+// 1. Masukkan pesan teks yang akan didekripsi.
+// 2. Masukkan kunci untuk Shift.
+// 3. Untuk setiap karakter dalam pesan teks, cari posisi dalam alfabet.
+// 4. Kurangi kunci Shift dari posisi alfabet untuk menghasilkan posisi baru.
+// 5. Ganti karakter dengan posisi baru yang telah ditentukan.
+// 6. Tampilkan hasil dekripsi.
+// 
+// Algoritma Vigenere Cipher
+// 
+// Langkah Manual:
+// 1. Tentukan kunci untuk enkripsi.
+// 2. Cari posisi dari karakter dalam kunci.
+// 3. Cari posisi dari karakter dalam pesan teks.
+// 4. Tambahkan posisi dari karakter dalam kunci dengan posisi karakter dalam pesan teks.
+// 5. Ganti karakter dengan posisi baru yang telah ditentukan.
+// 
+// Encrypt:
+// 1. Masukkan pesan teks yang akan dienkripsi.
+// 2. Masukkan kunci untuk enkripsi.
+// 3. Untuk setiap karakter dalam pesan teks, cari posisi dalam alfabet.
+// 4. Cari posisi dari karakter dalam kunci.
+// 5. Tambahkan posisi dari karakter dalam kunci dengan posisi karakter dalam pesan teks.
+// 6. Ganti karakter dengan posisi baru yang telah ditentukan.
+// 7. Tampilkan hasil enkripsi.
+// 
+// Decrypt:
+// 1. Masukkan pesan teks yang akan didekripsi.
+// 2. Masukkan kunci untuk dekripsi.
+// 3. Untuk setiap karakter dalam pesan teks, cari posisi dalam alfabet.
+// 4. Cari posisi dari karakter dalam kunci.
+// 5. Kurangi posisi dari karakter dalam kunci dari posisi karakter dalam pesan teks.
+// 6. Ganti karakter dengan posisi baru yang telah ditentukan.
+// 7. Tampilkan hasil dekripsi.
