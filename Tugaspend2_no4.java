d47ec5cad1e14d5b0ccb50c1b2ba8bfab736cde0
Achmad Ali Akbar <114000864+barcodew@users.noreply.github.com>
2023-10-17 19:38:46 +0800

    Update Tugaspend2_no4.java

--- Tugaspend2_no4.java
+++ Tugaspend2_no4.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class TugasPend24 {
 
-public class Main {
+
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -74,4 +74,4 @@ public class Main {
 }
 
 }
-}
+
